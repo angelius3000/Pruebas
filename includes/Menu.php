45aabe9b6d423acc9a0c871d2762aa188cfe8097
Arturo J. Guevara <aguevara@studioa.com.mx>
2024-02-26 11:33:20 -0700

    Se creó el menu separado ... el main.php y se quitaron los links que no servian

---
+++ includes/Menu.php
@@ -0,0 +1,19 @@
+<div class="app-menu">
+    <ul class="accordion-menu">
+        <!--   <li class="sidebar-title">
+            Apps
+        </li> -->
+        <li>
+            <a href="main.php"><i class="material-icons-two-tone">dashboard</i>Dashboard</a>
+        </li>
+        <li>
+            <a href="Repartos.php"><i class="material-icons-two-tone">inbox</i>Reparto</a>
+        </li>
+        <li>
+            <a href="Usuarios.php"><i class="material-icons-two-tone">inbox</i>Usuarios</a>
+        </li>
+
+
+
+    </ul>
+</div>
