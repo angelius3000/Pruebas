f00c701a6d9276cb5561860a952db482b942d512
Arturo J. Guevara <aguevara@studioa.com.mx>
2024-02-26 11:59:17 -0700

    Se creo la estructura de archivos para la aplicacion de Edicio Reparto

---
+++ Connections/Con.php
@@ -0,0 +1,25 @@
+<?php
+/* Database connection start */
+if ($_SERVER['HTTP_HOST'] == "local.edison:8888") {
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "root";
+    $dbname = "Edison";
+} else {
+
+    /* Database connection start */
+    $servername = "localhost:3306";
+    $username = "registromedico";
+    $password = "";
+    $dbname = "";
+}
+
+$conn = mysqli_connect($servername, $username, $password, $dbname) or die("Connection failed: " . mysqli_connect_error());
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$ParaChecar = "Aqui se conecto";
