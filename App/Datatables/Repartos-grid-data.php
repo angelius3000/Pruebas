fdde334b6e7387a940eac0bc6efbf154838ad990
Arturo J. Guevara <aguevara@studioa.com.mx>
2024-04-01 12:10:57 -0600

    Cambios de Minusculas a Mayusculas

--- App/Datatables/Repartos-grid-data.php
+++ App/Datatables/Repartos-grid-data.php
@@ -123,12 +123,12 @@ while ($row = mysqli_fetch_array($query)) {  // preparing an array ... Preparand
     $data[] = $nestedData;
 }
 
-
 $json_data = array(
     "draw" => intval($requestData['draw']),   // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw. 
     "recordsTotal"    => intval($totalData),  // total number of records
     "recordsFiltered" => intval($totalFiltered), // total number of records after searching, if there is no searching then totalFiltered = totalData
-    "data"            => $data
+    "data"            => $data  // total data array,
+
 );
 
 echo json_encode($json_data);  // send data as json format
