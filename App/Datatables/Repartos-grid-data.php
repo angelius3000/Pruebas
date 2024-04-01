84ab2a5edf4ca6baef91fd74d7cce979332338df
Arturo J. Guevara <aguevara@studioa.com.mx>
2024-04-01 11:24:22 -0600

    Se corrigieron algunos datos de los datagrids

--- App/Datatables/Repartos-grid-data.php
+++ App/Datatables/Repartos-grid-data.php
@@ -125,8 +125,7 @@ $json_data = array(
     "draw" => intval($requestData['draw']),   // for every request/draw by clientside , they send a number as a parameter, when they recieve a response/data they first check the draw number, so we are sending same number in draw. 
     "recordsTotal"    => intval($totalData),  // total number of records
     "recordsFiltered" => intval($totalFiltered), // total number of records after searching, if there is no searching then totalFiltered = totalData
-    "data"            => $data,   // total data array,
-    'NombreCongreso' => $rowCongreso["Congreso"]
+    "data"            => $data
 );
 
 echo json_encode($json_data);  // send data as json format
