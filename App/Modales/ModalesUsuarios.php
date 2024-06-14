5ba2a3bc37203d6a6ebfb31c66de621ecca27141
MiguelCH85 <miguel.chavez85@hotmail.com>
2024-06-14 17:19:39 -0600

    Se hace una corrección en el codigo que impedía que funcionara el buscador en el selector de clientes del modal de agregar usuarios

--- App/Modales/ModalesUsuarios.php
+++ App/Modales/ModalesUsuarios.php
@@ -37,9 +37,8 @@
                                             <div class="row" style="display:none" id="ClientesEscondidos">
                                                 <div class="col-lg-12 col-sm-12 mb-4">
                                                     <label for="CLIENTEID" class="form-label">Cliente ID</label>
-                                                    <!-- <input type="text" class="form-control" id="CLIENTEID" autocomplete="off" placeholder="0000" name="CLIENTEID" required> -->
                                                     <select class="form-select select2" name="CLIENTEID" id="CLIENTEID" aria-label="Default select example" required>
-                                                        <option value="">Selecciona cliente</option>
+                                                        <option selected>Selecciona cliente</option>
 
                                                         <?php while ($row_clientes = mysqli_fetch_assoc($clientes)) { ?>
 
