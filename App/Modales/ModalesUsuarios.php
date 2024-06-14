1506cfe26ab7fc4fa2f53aaca39aad07b8d4688b
MiguelCH85 <miguel.chavez85@hotmail.com>
2024-06-14 17:13:42 -0600

    Se vuelve a cambiar el tipo de captura del cliente en el modal de agregar usuarios de input a selector con buscador

--- App/Modales/ModalesUsuarios.php
+++ App/Modales/ModalesUsuarios.php
@@ -37,8 +37,8 @@
                                             <div class="row" style="display:none" id="ClientesEscondidos">
                                                 <div class="col-lg-12 col-sm-12 mb-4">
                                                     <label for="CLIENTEID" class="form-label">Cliente ID</label>
-                                                    <input type="text" class="form-control" id="CLIENTEID" autocomplete="off" placeholder="0000" name="CLIENTEID" required>
-                                                    <!-- <select class="form-select" name="CLIENTEID" id="CLIENTEID" aria-label="Default select example" required>
+                                                    <!-- <input type="text" class="form-control" id="CLIENTEID" autocomplete="off" placeholder="0000" name="CLIENTEID" required> -->
+                                                    <select class="form-select select2" name="CLIENTEID" id="CLIENTEID" aria-label="Default select example" required>
                                                         <option value="">Selecciona cliente</option>
 
                                                         <?php while ($row_clientes = mysqli_fetch_assoc($clientes)) { ?>
@@ -55,8 +55,7 @@
                                                         mysqli_data_seek($clientes, 0);
 
                                                         ?>
-
-                                                    </select> -->
+                                                    </select>
                                                 </div>
 
                                             </div>
