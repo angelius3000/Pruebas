7dfe33817599eb5251cb67d2aed8b1431e1ea976
Arturo J. Guevara <aguevara@studioa.com.mx>
2024-07-11 12:22:41 -0600

    Se corrigio el FAVICON en el Index de la aplicacion

--- index.php
+++ index.php
@@ -27,8 +27,25 @@
     <link href="assets/css/main.css" rel="stylesheet">
     <link href="assets/css/custom.css" rel="stylesheet">
 
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/images/neptune.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="assets/images/neptune.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="App/Graficos/Favicon/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="App/Graficos/Favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="App/Graficos/Favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="App/Graficos/Favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="App/Graficos/Favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="App/Graficos/Favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="App/Graficos/Favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="App/Graficos/Favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="App/Graficos/Favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="144x144" href="App/Graficos/Favicon/android-icon-144x144.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="App/Graficos/Favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="App/Graficos/Favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="App/Graficos/Favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="App/Graficos/Favicon/favicon-16x16.png">
+    <link rel="manifest" href="App/Graficos/Favicon/manifest.json">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="App/Graficos/Favicon/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
