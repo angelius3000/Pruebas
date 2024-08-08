4ed36324b7db56e046078c61af98f63e0bb981e4
JZEDSIST1\Edison <miguel.chavez85@hotmail.com>
2024-08-08 11:35:33 -0600

    Se actualiza librería

--- Repartos.php
+++ Repartos.php
@@ -290,7 +290,7 @@ $totalRows_Solicitantes = mysqli_num_rows($Solicitantes);
 
     <!-- DataTables Buttons JS -->
     <script type="text/javascript" charset="utf8" src="assets/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/1.7.0/js/buttons.html5.min.js"></script>
+    <script type="text/javascript" charset="utf8" src="assets/js/buttons.html5.min.js"></script>
     <!-- JSZip for Excel export -->
     <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
     <!-- pdfmake for PDF export -->
