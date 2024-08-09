380ae969b06528fc7182f5f36bd3819f503a24fe
JZEDSIST1\Edison <miguel.chavez85@hotmail.com>
2024-08-09 12:48:16 -0600

    Se ajusta serverupdateusuarios ya que no cambiaba a tipo de ususario cliente

--- App/Server/ServerUpdateUsuarios.php
+++ App/Server/ServerUpdateUsuarios.php
@@ -10,7 +10,7 @@ $ApellidoMaternoEditar = mysqli_real_escape_string($conn, $_POST['ApellidoMatern
 $emailEditar = mysqli_real_escape_string($conn, $_POST['emailEditar']);
 $TelefonoEditar = mysqli_real_escape_string($conn, $_POST['TelefonoEditar']);
 $USUARIOIDEditar = mysqli_real_escape_string($conn, $_POST['USUARIOIDEditar']);
-$CLIENTEIDEditar = mysqli_real_escape_string($conn, $_POST['CLIENTEIDEditar']);
+$CLIENTEID = isset($_POST['CLIENTEID']) ? mysqli_real_escape_string($conn, $_POST['CLIENTEID']) : 0;
 
 // Build the base query
 $sql = "UPDATE usuarios SET 
