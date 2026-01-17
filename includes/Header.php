bbe03cfd61d1fc0fa078d1f549e76d4e0d16a940
angelius3000 <160941211+angelius3000@users.noreply.github.com>
2026-01-17 11:39:09 -0700

    Add gradient theme selector

--- includes/Header.php
+++ includes/Header.php
@@ -15,6 +15,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+    <link href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" rel="stylesheet">
     <link href="assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/plugins/perfectscroll/perfect-scrollbar.css" rel="stylesheet">
     <link href="assets/plugins/pace/pace.css" rel="stylesheet">
